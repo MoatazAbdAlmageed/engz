2aa04767bd83920cefdf296d819d34d4c08a9a16
MoatazAbdAlmageed <moataz@wuilt.com>
2020-07-07 19:09:08 +0200

    delete task

--- web/src/Tasks.js
+++ web/src/Tasks.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 require("dotenv").config();
 function Tasks(props) {
-  const getTasksUrl = `${process.env.REACT_APP_API_URL}/tasks/${props.type}`;
-  const createTasksUrl = `${process.env.REACT_APP_API_URL}/tasks/create`;
+  const REACT_APP_API_URL = `${process.env.REACT_APP_API_URL}/tasks/`;
   console.log("🚀🚀🚀🚀🚀🚀 this.props");
   console.log(props);
   console.log("----------------------------------------------------");
@@ -11,13 +10,13 @@ function Tasks(props) {
   const [task, setTask] = useState([]);
 
   const getTasks = async () => {
-    const tasksApi = await fetch(getTasksUrl);
+    const tasksApi = await fetch(`${REACT_APP_API_URL}/${props.type}`);
     const tasksArray = await tasksApi.json();
     setTasks(tasksArray);
   };
 
   const saveTaskAPI = async () => {
-    const taskApi = await fetch(createTasksUrl, {
+    const taskApi = await fetch(`${REACT_APP_API_URL}/create`, {
       method: "POST",
       body: JSON.stringify({ title: task }),
       headers: { "Content-Type": "application/json" },
@@ -28,9 +27,19 @@ function Tasks(props) {
       setTask("");
     }
   };
+
+  const deleteTaskAPI = async (task) => {
+    const taskApi = await fetch(`${REACT_APP_API_URL}/delete/${task._id}`, {
+      method: "DELETE",
+    });
+    const taskData = await taskApi.json();
+    if (taskData.statusCode === 200) {
+      getTasks();
+    }
+  };
   useEffect(() => {
     getTasks();
-  }, [getTasksUrl, createTasksUrl]);
+  }, [REACT_APP_API_URL]);
 
   return (
     <>
@@ -41,10 +50,15 @@ function Tasks(props) {
         placeholder="task title here"
       />
       <button onClick={saveTaskAPI}>Save</button>
-      <h2>{props.type.toUpperCase()} Tasks</h2>
+      <h2>
+        {props.type.toUpperCase()} Tasks {tasks.length}{" "}
+      </h2>
       <ol>
         {tasks.map((task) => (
-          <li key={task._id}>{task.title}</li>
+          <li key={task._id}>
+            {task.title}{" "}
+            <button onClick={(e) => deleteTaskAPI(task)}>Delete</button>{" "}
+          </li>
         ))}
       </ol>
     </>
